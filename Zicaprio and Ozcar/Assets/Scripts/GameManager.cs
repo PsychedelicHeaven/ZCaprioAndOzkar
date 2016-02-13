18342268a184ecc1d211a35732b80c7ae8541b08
Abhishek Chandra <abhishekchandra2005@gmail.com>
2016-02-13 14:44:21 -0500

    basic shit

---
+++ Zicaprio and Ozcar/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+    public static GameManager Instance { get { return instance; } }
+
+    /// <summary> Skills </summary>
+    public enum skill { MethodActing, Looks, BodyLanguage, MartialArts}
+
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
