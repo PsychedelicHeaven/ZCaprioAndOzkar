3f412706657141abdb322daeb6ee3155681f2692
Abhishek Chandra <abhishekchandra2005@gmail.com>
2016-02-13 15:57:56 -0500

    global scripts

---
+++ Zicaprio and Ozcar/Assets/Scripts/Data/LifestyleData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class LifestyleData
+{
+    public GameManager.lifeStyle lifestyleType;
+    public float minReq;
+    public float current;
+}
---
+++ Zicaprio and Ozcar/Assets/Scripts/Global/Actor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Actor : MonoBehaviour
+{
+    private static Actor instance = null;
+    public static Actor Instance { get { return instance; } }
+
+    // ACTOR PROPERTIES
+
+    /// <summary> Actor level </summary>
+    public int level = 1;
+    /// <summary> Number of Fans </summary>
+    public int fans = 0;
+    /// <summary> $$$ </summary>
+    public float cash = 100;
+    /// <summary> Depression meter </summary>
+    public float depression = 0;
+    /// <summary> Actors current Skills </summary>
+    public List<SkillData> currentSkills = new List<SkillData>();
+    /// <summary> Lifestyle Requirements </summary>
+    [SerializeField]
+    private List<LifestyleData> lifestyleReqs = new List<LifestyleData>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
