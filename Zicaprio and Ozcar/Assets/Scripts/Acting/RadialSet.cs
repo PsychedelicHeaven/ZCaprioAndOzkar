54490dba14ea5c03ca68e2f43d21aada868e4f26
PsychedelicHeaven <vasill1992@gmail.com>
2016-02-13 19:14:02 -0500

    Mini game

---
+++ Zicaprio and Ozcar/Assets/Scripts/Acting/RadialSet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RadialSet : MonoBehaviour {
+
+    public float minangle;
+    public float maxangle;
+
+    private float angle;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        angle = maxangle - minangle;
+
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -minangle));
+
+        GetComponent<Image>().fillAmount = angle/360f;
+
+        
+
+    }
+
+    void SetAngle()
+    {
+        angle = maxangle - minangle;
+
+        GetComponent<Image>().fillAmount = angle / 360f;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, minangle));
+    }
+}
---
+++ Zicaprio and Ozcar/Assets/Scripts/Acting/RotateScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+    public float _Angle;
+    public float _Period;
+
+    private float _Time;
+
+    public float current_angle;
+
+    // Update is called noce per frame
+    void Update()
+    {
+        _Time = _Time + Time.deltaTime;
+        float phase = Mathf.Sin(_Time / _Period);
+
+        current_angle = -phase * _Angle + 90;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, phase * _Angle));
+    }
+}
