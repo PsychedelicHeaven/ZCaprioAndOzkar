99833a2f943e4b8036b969bcf0341ecb3e2dcfcc
PsychedelicHeaven <vasill1992@gmail.com>
2016-02-14 01:29:21 -0500

    Skills and sliders

--- Zicaprio and Ozcar/Assets/Scripts/Data/SkillData.cs
+++ Zicaprio and Ozcar/Assets/Scripts/Data/SkillData.cs
@@ -12,4 +12,17 @@ public class SkillData
     {
         return Actor.Instance.baseProficiencyReq * skillLevel * (skillLevel + 1) * 0.5f;
     }
+
+    public SkillData()
+    {
+        skillType = GameManager.skill.BodyLanguage;
+        proficiency = 1;
+        skillLevel = 1;
+    }
+    public SkillData(GameManager.skill _skillType, float _proficiency, int _skillLevel)
+    {
+        skillType = _skillType;
+        proficiency = _proficiency;
+        skillLevel = _skillLevel;
+    }
 }
