b0fefda7286d44bfecac695b1d2f1ab28ba25ef1
PsychedelicHeaven <vasill1992@gmail.com>
2016-02-13 21:45:32 -0500

    Complete Screen

--- Zicaprio and Ozcar/Assets/Scripts/Acting/RadialScript.cs
+++ Zicaprio and Ozcar/Assets/Scripts/Acting/RadialScript.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class RadialScript : MonoBehaviour {
 
     public RadialSet orange;
     public RadialSet green;
+    public RotateScript arrow;
 
 
     public int difficulty;
@@ -19,10 +21,19 @@ public class RadialScript : MonoBehaviour {
     public float length_orange;
     public float length_green;
 
+
+    public float score;
+
+
+    public int amount_of_scenes;
+
+    public Text score_text;
+    public GameObject CompleteScreen;
+    public int current_scene;
     // Use this for initialization
     void Start () {
-	
-	}
+        ClearScene();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,10 +41,34 @@ public class RadialScript : MonoBehaviour {
         {
             SetScene(difficulty);
         }
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            score += GetScore();
+            current_scene++;
+            if(current_scene > amount_of_scenes)
+            {
+                CompleteScreen.SetActive(true);
+                score_text.text = "Score = " + score;
+                ClearScene();
+                transform.parent.gameObject.SetActive(false);
+            }
+            else
+            {
+                SetScene(difficulty);
+            }
+        }
+
     }
 
-    void SetScene(int _difficulty) // _difficulty : 0 - 100
+    void ClearScene() // _difficulty : 0 - 100
+    {
+        current_scene = 0;
+        score = 0;
+        SetScene(difficulty);
+    }
+
+        void SetScene(int _difficulty) // _difficulty : 0 - 100
     {
         orange.minangle = Random.Range(min_orange, 180 - 2*min_orange - 100 + difficulty);
         orange.maxangle = orange.minangle + 100 + min_orange - difficulty;
@@ -44,5 +79,21 @@ public class RadialScript : MonoBehaviour {
         green.maxangle = green.minangle + 50 + min_green - difficulty/2f;
 
         length_green = green.maxangle - green.minangle;
+    }
+
+    float GetScore() // _difficulty : 0 - 100
+    {
+        if(arrow.current_angle > green.minangle && arrow.current_angle < green.maxangle)
+        {
+            return (1f);
+        }
+        else if(arrow.current_angle > orange.minangle && arrow.current_angle < orange.maxangle)
+        {
+            return (0.7f);
+        }
+        else
+        {
+            return (0.2f);
+        }
     }
 }
