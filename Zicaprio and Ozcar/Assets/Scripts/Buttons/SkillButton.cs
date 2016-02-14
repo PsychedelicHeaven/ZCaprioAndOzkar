400a02e7238a6889d30f781cd6abf83c4f6c6c01
PsychedelicHeaven <vasill1992@gmail.com>
2016-02-13 23:34:02 -0500

    Adding buttons

---
+++ Zicaprio and Ozcar/Assets/Scripts/Buttons/SkillButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillButton : MonoBehaviour {
+
+    GameManager.skill skill_to_do;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void ActivateSkill()
+    {
+        Actor.Instance.AddSkill(skill_to_do);
+    }
+    
+}
